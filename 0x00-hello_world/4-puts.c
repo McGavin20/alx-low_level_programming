5fe31f3fcaa6ff0a2f7a51633d0606dc3f81cc35
Sharma <smuridzo20@gmail.com>
2022-09-08 07:09:09 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry Point prints out the phrase
+ *
+ * prints phrase Programming is like building a multilingual puzzle
+ *
  * Return: 0 when true(success)
  */
 int main(void) 
