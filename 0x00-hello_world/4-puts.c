a34a2bfc7c8250cf744a70b33839961bef9efd27
Sharma <smuridzo20@gmail.com>
2022-09-08 05:43:57 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - Entry Point
+/**
+ * main - Entry Point prints out the phrase
  * Return: 0 when true(success)
  */
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
- 	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
