bf0eb42738167f5852038adb88a37f68f34905bb
Sharma <smuridzo20@gmail.com>
2022-09-09 05:26:59 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,16 @@ int main(void)
 	int x;
 
 	for (x = 0; x < 10; x++)
+	{
 		putchar(a);
 		a++;
+	}
 	a = '1';
-	for (x = 0; x < 6; x++;)
+	for (x = 0; x < 6; x++)
+	{
 		putchar('0' + a);
 		a++;
+	}
 	putchar('\n');
 	return (0);
 }
