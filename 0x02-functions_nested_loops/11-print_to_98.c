0ccb6b7317351e5d8ff6ce8f87573fc393acf0c7
Sharma <smuridzo20@gmail.com>
2022-09-13 08:19:20 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,30 @@
 #include "main.h"
+#include<stdio.h>
 /** 
  * print_to_98 - prints from 0 to 98 natural numbers
  * @n: The number to begin counting at
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
-	}
+	int i, j;
 
-	else
+	if (n <= 98)
 	{
-		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%d\n", n);
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
