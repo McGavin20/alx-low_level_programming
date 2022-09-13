c512746e424c7f13b05dceaf082368803717632c
Sharma <smuridzo20@gmail.com>
2022-09-13 08:09:30 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - adds two integer, num1 & num2
  * @num1: first integer
  * @num2: second integer
- * return: sum of num1 & num2
+ * Return: sum of num1 & num2
  */
 int add(int num1, int num2)
 {
