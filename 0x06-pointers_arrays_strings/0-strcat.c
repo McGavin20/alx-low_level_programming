19a76ee889fbeaf65390f197a348485d13893768
Sharma <smuridzo20@gmail.com>
2022-09-21 02:17:30 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: 0 Always
+ */
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest, src);
+
+	return (0);
+}
